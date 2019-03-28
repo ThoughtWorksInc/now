7d8c9738bd6b1f83a4f2b592fb6b5146ef09b82f
Kapil Dube <kapil.dube@thoughtworks.com>
2019-03-28 14:50:49 +0100

    :tada: introduced gatsby

---
+++ gatsby.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  siteMetadata: {
+    title: `ThoughtWorks - NOW`,
+    description: `ThoughtWorks presents: Network of women!`,
+    author: `@thoughtworks_de`
+  },
+  plugins: []
+};
