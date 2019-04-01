4dfa26c253461d61c53e8b37f4566af626f0ed07
Kapil Dube <kapil.dube@thoughtworks.com>
2019-04-01 16:13:50 +0200

    links need more beautification, parking for now

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -2,60 +2,35 @@ import React from "react";
 import logo from "../../images/tw-logo.png";
 import "./header.css";
 
-class MobileNavContainer extends React.Component {
-  state = {
-    showExpandedMenu: true
-  };
+const constructMenu = links => {
+  return (
+    <>
+      <input id="menu-toggle" type="checkbox" />
+      <label class="menu-button-container" for="menu-toggle">
+        <div class="menu-button" />
+      </label>
+      <ul class="menu">
+        {links.map(item => (
+          <li>
+            <a key={item.target} href={"#" + item.target}>
+              {item.name}
+            </a>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+};
 
-  toggleMenu = () => {
-    this.setState({ showExpandedMenu: !this.state.showExpandedMenu });
-  };
-
-  render() {
-    return (
-      <>
-        <input
-          id="menu-toggle"
-          type="checkbox"
-          onClick={() => this.toggleMenu()}
-        />
-        <label class="menu-button-container" for="menu-toggle">
-          <div class="menu-button" />
-        </label>
-
-        {this.props.children}
-      </>
-    );
-  }
-}
-
-class Header extends React.Component {
-  render() {
-    return (
-      <section class="top-nav">
-        <img src={logo} className="headerLogo" />
-        <MobileNavContainer>
-          <ul class="menu">
-            <li>
-              <a key="about" href="#about">
-                About
-              </a>
-            </li>
-            <li>
-              <a key="schedule" href="#schedule">
-                Schedule
-              </a>
-            </li>
-            <li>
-              <a key="speakers" href="#speakers">
-                Speakers
-              </a>
-            </li>
-          </ul>
-        </MobileNavContainer>
-      </section>
-    );
-  }
-}
+const Header = ({ title, campaignDate, links }) => {
+  return (
+    <section class="top-nav">
+      <img src={logo} className="headerLogo" alt="ThoughtWorks logo" />
+      {title && <h3>{title}</h3>}
+      {campaignDate && <h4>{campaignDate}</h4>}
+      {links && constructMenu(links)}
+    </section>
+  );
+};
 
 export default Header;
--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -2,9 +2,17 @@ import React from "react";
 import Header from "./header/header";
 import "./layout.css";
 
+const links = [
+  { target: "about", name: "About" },
+  { target: "schedule", name: "Schedule" },
+  { target: "speakers", name: "Speakers" },
+  { target: "Register", name: "Register" },
+  { target: "venue", name: "Venue" }
+];
+
 const Layout = ({ children }) => (
   <div className="site-layout">
-    <Header />
+    <Header title="NOW Germany" campaignDate="21 May 2019" links={links} />
     <div className="container">
       <h1>This is my section</h1>
       {children}
