561147dce976299b9ecd6e9f5d03a6c76d825af8
Kapil Dube <kapil.dube@thoughtworks.com>
2019-04-03 14:05:26 +0200

    Initial version of header with Emotion

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,36 +1,150 @@
 import React from "react";
 import logo from "../../images/tw-logo.png";
-import "./header.css";
+import styled from "@emotion/styled";
+import { Link } from "gatsby";
+import colors from "../../themes/colors";
 
-const constructMenu = links => {
+const Header = styled("header")`
+  background-color: white;
+  left: 0;
+  position: fixed;
+  right: 0;
+  top: 0;
+  z-index: 1000;
+  display: flex;
+  flex-direction: row
+  ::before,
+  ::after {
+    content: "";
+    display: block;
+    position: absolute;
+    right: 0;
+    height: 1px;
+    width: calc(100% - 30px);
+    z-index: 1;
+  }
+  ::before {
+    top: calc(100% - 1px);
+  }
+`;
+
+const SkipToContent = styled("a")`
+  transition: none;
+  clip: rect(0 0 0 0);
+  overflow: hidden;
+  position: absolute;
+  height: 1px;
+  width: 1px;
+  :focus,
+  :active,
+  :hover {
+    clip: auto;
+    width: auto;
+    height: auto;
+    border-radius: 0;
+    padding: 0.5rem 1rem;
+    z-index: 5000;
+  }
+
+  ::before {
+    top: calc(100% - 1px);
+    background-color: ${colors.primary};
+  }
+  ::after {
+    top: 100%;
+    background-color: ${colors.grayAlpha};
+  }
+`;
+
+const Nav = styled("nav")`
+  display: flex;
+  font-size: 0.875rem;
+  justify-content: flex-end;
+  margin: 0;
+  position: relative;
+  z-index: 5;
+`;
+
+const NavLink = styled(Link)`
+  border: 2px solid transparent;
+  border-radius: 0;
+  color: "black";
+  font-weight: 900;
+  line-height: 1.25;
+  margin: 0;
+  padding: calc(0.5rem - 2px) 0.25rem;
+  text-decoration: none;
+  text-transform: uppercase;
+  transition-property: color;
+  &.active {
+    color: ${colors.primary};
+  }
+  &.hiddenSmall {
+    display: none;
+    @media (min-width: 414px) {
+      display: inline-block;
+    }
+  }
+  @media (min-width: 320px) {
+    padding-left: 0.625rem;
+    padding-right: 0.625rem;
+  }
+  :focus,
+  :active,
+  :hover {
+    background-color: transparent;
+    border-radius: 0;
+    color: ${colors.primaryDark};
+  }
+  :focus {
+    border-color: ${colors.darkest};
+  }
+`;
+
+const LogoLink = styled(NavLink)`
+  border: 0;
+
+  padding: 0;
+  position: relative;
+
+  z-index: 10;
+`;
+
+const Logo = styled("img")`
+  padding: 0;
+  position: absolute;
+  width: 10vw;
+  z-index: 2;
+`;
+
+const SiteHeader = ({ title, campaignDate, links }) => {
   return (
-    <>
-      <input id="menu-toggle" type="checkbox" />
-      <label className="menu-button-container" htmlFor="menu-toggle">
-        <div className="menu-button" />
-      </label>
-      <ul className="menu">
+    <Header role="banner">
+      <SkipToContent href="#" id="skip-navigation">
+        Skip to Content
+      </SkipToContent>
+      <LogoLink to="/">
+        <Logo
+          src={logo}
+          alt="ThoughtWorks presents: Network of Women"
+          // This keeps the logo from flashing at full-width on fresh loads.
+          // style={{ maxWidth: "42px" }}
+        />
+      </LogoLink>
+      <Nav>
         {links.map(item => (
-          <li key={item}>
-            <a href={"#" + item}>{item}</a>
-          </li>
+          <NavLink
+            key={item}
+            to={`/#${item}`}
+            className="text-sharp"
+            activeClassName="active"
+          >
+            {item}
+          </NavLink>
         ))}
-      </ul>
-    </>
+      </Nav>
+    </Header>
   );
 };
 
-const Header = ({ title, campaignDate, links }) => {
-  return (
-    <section className="top-nav">
-      <img src={logo} className="headerLogo" alt="ThoughtWorks logo" />
-      <div style={{ display: "inline" }}>
-        {title && <h3>{title}</h3>}
-        {campaignDate && <h4>{campaignDate}</h4>}
-      </div>
-      {links && constructMenu(links)}
-    </section>
-  );
-};
-
-export default Header;
+export default SiteHeader;
--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -1,10 +1,10 @@
 import React from "react";
-import Header from "./header/header";
+import SiteHeader from "./header/header";
 import "./layout.css";
 
 const Layout = ({ children, menuEntries }) => (
-  <div className="site-layout">
-    <Header title="N.O.W. Germany" links={menuEntries} />
+  <div>
+    <SiteHeader title="N.O.W. Germany" links={menuEntries} />
     <div className="container">{children}</div>
     <footer>This is my footer</footer>
   </div>
